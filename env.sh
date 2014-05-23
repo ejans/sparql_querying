9bcd3d236363a8b2e37074e4455741a1a50d81f7
Evert Jans <evert.jans@mech.kuleuven.be>
2014-05-23 12:15:33 +0200

    pnp ontology test working

--- env.sh
+++ env.sh
@@ -2,4 +2,5 @@
 
 # Point to the redland.so file
 #export LUA_CPATH="$HOME/redland-bindings/lua/redland.so"
-export LUA_CPATH="$HOME/Documents/builds/redland-bindings/lua/redland.so"
+#export LUA_CPATH="$HOME/Documents/builds/redland-bindings/lua/redland.so"
+export LUA_CPATH="$HOME/Downloads/dajobe/redland-bindings/lua/redland.so"
